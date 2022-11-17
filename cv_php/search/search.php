01a3920fd669d5d5455c38598b412abb027b890b
ibraheem alabed <114851733+ibraheem-alabed@users.noreply.github.com>
2022-11-17 14:20:47 +0300

    Update search.php

--- cv_php/search/search.php
+++ cv_php/search/search.php
@@ -1,1 +1,12 @@
 <?php
+$cvs = json_decode(file_get_contents('../data/old_cv.json'));
+
+
+$search_term = !empty($_GET['s']) ? $_GET['s'] : null;
+$final_cvs = array();
+
+foreach ($cvs as $cv) {
+    if (strpos($cv->description, $search_term) !== false) {
+        $cvs[] = $cv;
+    }
+}
